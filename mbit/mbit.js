a30d02351b0817aee805ee687c946e01772a9f9a
theonewhobuilds8410 <lekrejci312@gmail.com>
2025-11-19 23:11:58 -0600

    Update mbit.js

--- mbit/mbit.js
+++ mbit/mbit.js
@@ -34,112 +34,86 @@ sendBtn.addEventListener('click', async () => {
 
 const snakeCode = `
 // Snake Game
-function moveForward () {
-    basic.pause(wait)
-    x1 = arr[0] % 5
-    y1 = Math.trunc(arr[0] / 5)
-    if (x1 + x_add < 0) {
-        x1 = 4
-    } else {
-        x1 = (x1 + x_add) % 5
-    }
-    if (y1 + y_add < 0) {
-        y1 = 4
-    } else {
-        y1 = (y1 + y_add) % 5
-    }
-    arr.insertAt(0, x1 + y1 * 5)
-    arr.removeAt(arr.length - 1)
-    
-}
-function generateImage () {
-    basic.clearScreen()
-    for (let i = 0; i <= arr.length - 1; i++) {
-        x = arr[i] % 5
-        y = Math.trunc(arr[i] / 5)
-        led.plot(x, y)
-    }
-}
-function showTreasure(){
-    led.plot(treasure_x,treasure_y)
-    x = arr[0]%5
-    y = Math.trunc(arr[0]/5)
-    if(x==treasure_x&&y==treasure_y){
-        arr.push(arr[0])
-        treasure_x = randint(0,4)
-        treasure_y = randint(0,4)
-    }
-}
-let treasure_x = randint(0,4)
-let treasure_y = randint(0,4)
-let y = 0
-let x = 0
-let y1 = 0
-let x1 = 0
-let arr: number[] = []
-let wait = 0
-let x_add = 0
-let y_add = 1
-wait = 500
-arr = [0]
-basic.forever(function () {
-    generateImage()
-    moveForward()
-})
-basic.forever(function(){
-    showTreasure()
-})
-input.onButtonPressed(Button.B,function(){
-    if (x_add == 0) {
-        x_add = 1
-        y_add = 0
-    } else if (y_add == 0) {
-        x_add = 0
-        y_add = 1
-    } 
-})
-input.onButtonPressed(Button.AB,function(){
-    arr=[0]
-})
-input.onButtonPressed(Button.A, function () {
-    if (x_add == 0) {
-        x_add = -1
-        y_add = 0
-    } else if ( y_add == 0) {
-        x_add = 0
-        y_add = -1
-    } 
-})
+from microbit import *
+import random
+
+# Snake setup
+snake = [(2, 2)]  # start in middle
+dx, dy = 0, 1     # initial direction: down
+wait = 500        # milliseconds
+
+# Treasure
+apple = (random.randint(0, 4), random.randint(0, 4))
+
+def rotate_left(dx, dy):
+    return -dy, dx
+
+def rotate_right(dx, dy):
+    return dy, -dx
+
+while True:
+    # move snake
+    head_x, head_y = snake[0]
+    new_head = ((head_x + dx) % 5, (head_y + dy) % 5)
+    snake = [new_head] + snake  # prepend new head safely
+
+    # eat apple
+    if new_head == apple:
+        # grow snake (do nothing, keep tail)
+        apple = (random.randint(0, 4), random.randint(0, 4))
+    else:
+        # remove tail
+        snake = snake[:-1]
+
+    # draw
+    display.clear()
+    for x, y in snake:
+        display.set_pixel(x, y, 9)
+    display.set_pixel(apple[0], apple[1], 5)
+
+    sleep(wait)
+
+    # buttons
+    if button_a.was_pressed():
+        dx, dy = rotate_left(dx, dy)
+    if button_b.was_pressed():
+        dx, dy = rotate_right(dx, dy)
 `;
 
 const pongCode = `
 // Pong Game
-let bar_x = 0
-let point = 0
-let interval = 0
-let interval_step = 0
-let ball_x = 0
-let ball_y = 0
-let ball_dx = 0
-let ball_dy = 0
-let in_game = false
-// 往左
-input.onButtonPressed(Button.A, function () {
-    if (bar_x > 0) {
-        led.unplot(bar_x + 1, 4)
-        bar_x = bar_x - 1
-        led.plot(bar_x, 4)
-    }
-})
-// 往右
-input.onButtonPressed(Button.B, function () {
-    if (bar_x < 3) {
-        led.unplot(bar_x, 4)
-        bar_x = bar_x + 1
-        led.plot(bar_x + 1, 4)
-    }
-})
-basic.forever(function () {
+from microbit import *
+
+# ---- Variables ----
+bar_x = 0
+point = 0
+interval = 0
+interval_step = 0
+ball_x = 0
+ball_y = 0
+ball_dx = 0
+ball_dy = 0
+in_game = False
+
+# ---- Functions for buttons ----
+def move_left():
+    global bar_x
+    if bar_x > 0:
+        display.set_pixel(bar_x + 1, 4, 0)  # unplot using 0
+        bar_x -= 1
+        display.set_pixel(bar_x, 4, 9)
+        display.set_pixel(bar_x + 1, 4, 9)
+
+def move_right():
+    global bar_x
+    if bar_x < 3:
+        display.set_pixel(bar_x, 4, 0)  # unplot using 0
+        bar_x += 1
+        display.set_pixel(bar_x, 4, 9)
+        display.set_pixel(bar_x + 1, 4, 9)
+
+# ---- Main game loop ----
+while True:
     point = 0
     interval = 500
     interval_step = 10
@@ -148,48 +122,50 @@ basic.forever(function () {
     ball_dx = -1
     ball_dy = -1
     bar_x = 0
-    basic.showString("GO!")
-    led.plot(ball_x, ball_y)
-    led.plot(bar_x, 4)
-    led.plot(bar_x + 1, 4)
-    in_game = true
-    while (in_game) {
-        if (ball_x + ball_dx > 4) {
-            ball_dx = ball_dx * -1
-        } else if (ball_x + ball_dx < 0) {
-            ball_dx = ball_dx * -1
-        }
-        if (ball_y + ball_dy < 0) {
-            ball_dy = ball_dy * -1
-        } else if (ball_y + ball_dy > 3) {
-            if (led.point(
-            ball_x + ball_dx,
-            ball_y + ball_dy
-            )) {
-                ball_dy = ball_dy * -1
-                point = point + 1
-                if (interval - interval_step >= 0) {
-                    interval = interval - interval_step
-                }
-            } else {
-                in_game = false
-            }
-        }
-        if (in_game) {
-            led.plot(
-            ball_x + ball_dx,
-            ball_y + ball_dy
-            )
-            led.unplot(ball_x, ball_y)
-            ball_x = ball_x + ball_dx
-            ball_y = ball_y + ball_dy
-            basic.pause(interval)
-        } else {
-            game.setScore(point)
-            game.gameOver()
-        }
-    }
-})
+    display.scroll("GO!")
+
+    # Draw initial paddle and ball
+    display.set_pixel(ball_x, ball_y, 9)
+    display.set_pixel(bar_x, 4, 9)
+    display.set_pixel(bar_x + 1, 4, 9)
+
+    in_game = True
+
+    while in_game:
+        # Handle buttons
+        if button_a.was_pressed():
+            move_left()
+        if button_b.was_pressed():
+            move_right()
+
+        # Check horizontal bounds
+        if ball_x + ball_dx > 4 or ball_x + ball_dx < 0:
+            ball_dx *= -1
+
+        # Check vertical bounds
+        if ball_y + ball_dy < 0:
+            ball_dy *= -1
+        elif ball_y + ball_dy > 3:
+            # Check paddle collision
+            if (ball_x + ball_dx == bar_x or ball_x + ball_dx == bar_x + 1):
+                ball_dy *= -1
+                point += 1
+                if interval - interval_step >= 0:
+                    interval -= interval_step
+            else:
+                in_game = False
+
+        if in_game:
+            # Move ball
+            display.set_pixel(ball_x + ball_dx, ball_y + ball_dy, 9)
+            display.set_pixel(ball_x, ball_y, 0)  # unplot using 0
+            ball_x += ball_dx
+            ball_y += ball_dy
+            sleep(interval)
+        else:
+            display.scroll("Score: {}".format(point))
+            sleep(1000)
+            display.clear()
 
 `;
 
@@ -206,4 +182,5 @@ document.getElementById("copySnake").addEventListener("click", () => copyToClipb
 document.getElementById("copyPong").addEventListener("click", () => copyToClipboard(pongCode));
 
 });
+
 
